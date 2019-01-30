b3b6ee174e9a9e2c85db12ee3b57c043cbd11527
yzh_Dian <2169218186@qq.com>
2019-01-30 12:12:00 +0800

    完成计时和Makefile

---
+++ source/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+double get_time()
+{
+    struct timeval t;
+    gettimeofday(&t,NULL);
+    return t.tv_sec + t.tv_usec/1000000.0;
+}
+
+
+int main(int argc, char const *argv[])
+{     
+      double t1,t2;
+      t1 = get_time();
+      printf("%f", t1);
+
+      for(int i = 0; i < 100; i++)
+      {
+            printf("");
+      }
+      t2 = get_time();
+      printf("runtime is %f\n",t2-t1);
+
+      return 0;
+}
