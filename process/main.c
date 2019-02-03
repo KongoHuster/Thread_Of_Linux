1623becbe5f099ee082c6b6ca6c77283cfb5a315
yzh_Dian <2169218186@qq.com>
2019-02-03 23:08:12 +0800

    在Linux下修复bug

--- process/main.c
+++ process/main.c
@@ -18,7 +18,6 @@ int main(int argc, char const *argv[])
     DIR *dir = opendir("./input/");
     struct dirent *entry;
 
-
     if (dir == NULL)
     {
         printf("opendir failed!");
@@ -27,34 +26,36 @@ int main(int argc, char const *argv[])
     else
     {
         //跳过..
-        entry = readdir(dir);
-        entry = readdir(dir);
-        char *outPath = (char *)malloc(strlen(fileOutputPath) + strlen(".txt") + 2); 
+        // entry = readdir(dir);
+        // entry = readdir(dir);
+        char *outPath = (char *)malloc(strlen(fileOutputPath) + strlen(".txt") + 2);
         char *inputPath = (char *)malloc(strlen(fileInputPath) + strlen("input.txt") + 2);
-        int Q = -1;
-        for(int i = 0; entry != NULL; i++)
+
+        for (int i = 0; entry != NULL; i++)
         {
             entry = readdir(dir);
 
             //判断是否entry为空
-            if (entry == NULL) {
+            if (entry == NULL)
+            {
 
-                printf("运行结束\n"); 
+                printf("运行结束\n");
                 closedir(dir);
                 break;
             }
-        
+
             sprintf(outPath, "%s%d%s", fileOutputPath, i, ".txt");
             sprintf(inputPath, "%s%s", fileInputPath, entry->d_name);
-            run_thread(inputPath, outPath); 
-            printf("\n\n");
-            // printf("Path is %s\n", outPath);
-            // printf("Path is %s\n", inputPath);
-            // printf("filename = %s\n", entry->d_name); //输出文件或者目录的名称
-            // printf("filetype = %d\n\n\n", entry->d_type);     //输出文件类型
-        }
+            sprintf(inputPath, "%s%s", fileInputPath, entry->d_name);
 
-        return 0;
+            if(strcmp(inputPath, "./input/.") != 0 & strcmp(inputPath, "./input/..") != 0)
+            {
+                run_thread(inputPath, outPath);
+                printf("\n\n");
+            }
+
+
+            return 0;
+        }
     }
 }
-
--- thread/time.c
+++ thread/time.c
@@ -4,6 +4,6 @@
 double get_time()
 {
     struct timeval t;
-    gettimeofday(&t,NULL);
+    gettimeofday(&t, 0);
     return t.tv_sec + t.tv_usec/1000000.0;
 }
